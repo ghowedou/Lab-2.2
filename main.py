ce31a4a8ff8c114f1a828717c523451aefde4a49
ghowedou <gilliandoucet@gmail.com>
2020-09-22 23:11:11 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+import numpy as np
+from array import *
+
+arrtemp = array('i',[])
+
+n = int(input('Enter the amount of temperatures you would like: '))
+
+for i in range(n):
+    temp = int(input('Enter the next value: '))
+    arrtemp.append(temp)
+
+print(arrtemp)
+
+arrfreq = array('q',[])
+
+m = int(input('Enter the amount of temperatures you would like: '))
+
+for q in range(m):
+    freq = int(input('Enter the next value: '))
+    arrfreq.append(freq)
+
+print(arrfreq)
