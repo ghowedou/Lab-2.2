3998a6cfad8b9bc7f36d73cf8382c02799105749
ghowedou <gilliandoucet@gmail.com>
2020-09-23 15:28:47 +0000

    Trying to make planck function

--- main.py
+++ main.py
@@ -1,12 +1,11 @@
 import numpy as np
 from array import *
-from PyAstronomy.pyasl import planck
 
 arrtemp = array('i',[])
 
-n = int(input('Enter the amount of temperatures you would like: '))
+m = int(input('Enter the amount of temperatures you would like: '))
 
-for i in range(n):
+for i in range(m):
     temp = int(input('Enter the next value: '))
     arrtemp.append(temp)
 
@@ -14,16 +13,18 @@ print(arrtemp)
 
 arrfreq = array('q',[])
 
-m = int(input('Enter the amount of temperatures you would like: '))
+n = int(input('Enter the amount of temperatures you would like: '))
 
-for q in range(m):
+for q in range(n):
     freq = int(input('Enter the next value: '))
     arrfreq.append(freq)
 
 print(arrfreq)
 
-def planck(n,m):
-    for (i,q) in range(n,m):
-        planck(arrtemp,arrfreq)
-        print(planck)
-    return 
+def planck(m,n):
+    h = 6.62606896**(−34)
+    c = 2.99792458**8
+    k = 1.3806504**(−23)
+    v = arrfreq
+    T = arrtemp
+    x = (2*h*(v**3/c**2))*(exp(hv/k*T)-1)**(-1)
